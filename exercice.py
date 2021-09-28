316b39044ee4e2d6f184b3cceb223a0e59dad0b9
Ayelander <89856450+Ayelander@users.noreply.github.com>
2021-09-28 10:36:07 -0400

    Exercices pour le chapitre 5 complets, sauf pour verify_ages - Tristan

--- exercice.py
+++ exercice.py
@@ -6,24 +6,88 @@ from typing import List
 
 
 def convert_to_absolute(number: float) -> float:
-    return 0
+    if number < 0:
+        number *= -1
+    return number
+
+# Solution alternative:
+# return number if number > 0 else number *= -1
+# Utile pour faire les choses en une seule ligne
+# Si le code est complexe, cette approche n'est pas à privilégier
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
+    for first_char in prefixes:
+        print(first_char + suffixe)
 
-    return [""]
+# Solution alternative:
+# liste_nom = []
 
+# for letter in prefixes:
+#   liste_nom.append(letter + suffixe)
+
+def is_prime(number: int) -> bool:
+    for i in range(2, round((number/2)+1)):
+        if number % i == 0:
+            return True
+    return False
 
 def prime_integer_summation() -> int:
-    return 0
+    n = int(input("How many primes do you want to sum?"))
+    i = 2 # 0 and 1 are not prime kekw
 
+    list_of_primes = []
+
+    nb_prime = 0
+
+    while nb_prime <= n:
+        if is_prime(i):
+            list_of_primes.append(i)
+            nb_prime += 1
+        i += 1
+    return sum(list_of_primes)
+
+# Solution alternative 1:
+
+# somme = 0
+
+# liste_premiers = [2, 3, 5, 7, 11, ...]
+
+# for nombre in liste_premiers:
+#    somme += nombre
+
+# Solution alternative 2:
+
+# somme = 0
+
+# liste_premiers = [2, 3, 5, 7, 11, ...]
+
+#   return sum(liste_premiers)
 
 def factorial(number: int) -> int:
-    return 0
+    result = 1
+    if number < 0:
+        print("This number has no factorial")
+    elif number == 0:
+        print("This number's factorial is 1")
+    elif number > 0:
+        for i in range(1,number+1):
+            result *= i
+    return result
 
+# Solution alternative:
+
+# while number > 1
+# result *= number
+# number -= 1
 
 def use_continue() -> None:
+    i = 1
+    for i in range (1,10):
+        if i == 5:
+            continue
+        print(i)
     pass
 
 
